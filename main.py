6ad4317bd1911b1e89b17a76ce3daad109c270ff
Asaduzzama-n <asad_uzzaman@outlook.com>
2023-09-09 11:36:59 +0600

    reade me file added

--- main.py
+++ main.py
@@ -1,12 +1,30 @@
 import time
 
 from donation.donation import Donation
+from registration.registration import Registration
+from report.report import Report
+# with Donation() as don:
+#     don.land_first_page()
+#     don.login_user("smridul191006@bscse.uiu.ac.bd", "Smridul!12@")
+#     don.view_campaign_card()
+#     current_donation = don.get_previous_donation_value()
+#     don.set_donation_amount(300)
+#     don.select_anonymous_checkbox(0)
+#     don.click_donate_button()
+#     don.set_card_info("4242424242424242", "1232", "133", "12121")
+#     don.donation_test()
+#     don.check_donation_update(current_donation, 300)
 
-with Donation() as don:
-    don.land_first_page()
-    don.login_user("smridul191006@bscse.uiu.ac.bd", "Smridul!12@")
-    time.sleep(2)
-    don.view_campaign_card()
-    don.donation_test(200)
+# with Registration() as reg:
+#     reg.land_registration_page()
+#     # reg.check_successful_registration("Asad", "as232@as.com", "Asf#25Gc")
+#     # reg.check_password()
+
+# with Report() as rep:
+#     rep.land_login_page()
+#     rep.login_user("asaduzzaman193146@gmail.com", "asadvai")
+#     time.sleep(4)
+#     rep.redirect_to_report_generation_page()
+
 while True:
     pass
